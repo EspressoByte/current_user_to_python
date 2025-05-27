6be041967927b5e30c6280da72836b963fff1ded
CodeNavigator <39248635+EspressoByte@users.noreply.github.com>
2025-05-27 11:57:44 -0400

    Update gatekeeper.c
    Correction to current users instead of root

--- gatekeeper.c
+++ gatekeeper.c
@@ -5,10 +5,11 @@
 #include <stdio.h>
 
 int main() {
-    setgid(0);
+    uid_t real_uid = getuid();  // 1. Capture real user ID BEFORE elevation
+
+    setgid(0);  // 2. Then elevate
     setuid(0);
 
-    uid_t real_uid = getuid();
     struct passwd *pw = getpwuid(real_uid);
     if (!pw) {
         fprintf(stderr, "Failed to get username for UID %d\n", real_uid);
@@ -17,6 +18,7 @@ int main() {
 
     char *username = pw->pw_name;
 
+    // 3. Call your protected script
     execl("/usr/bin/python3", "python3", "/opt/secret/secret_script.py", username, (char *)NULL);
 
     perror("execl");
